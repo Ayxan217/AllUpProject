936309b7258ed2a42b884122171ca41ec013ac38
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-12-08 17:18:40 +0400

    commit

--- AllUpProject/AllUpProject/Controllers/HomeController.cs
+++ AllUpProject/AllUpProject/Controllers/HomeController.cs
@@ -16,39 +16,20 @@ namespace AllUpProject.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             var products = _context.Products.Include(p => p.Category).ToList();
             var categories = _context.Categories.ToList();
 
-            List<Slider> sliders = new List<Slider>
+            HomeViewModel homeVM = new()
             {
-                new Slider
-                {
-                    Title = "Sony Bravia",
-                    Subtitle = "4K HDR Smart TV",
-                    Order = 1,
-                    Description = "Explore 360 Platform",
-                },
-                new Slider
-                {
-                    Title = "Fulldive VR",
-                    Subtitle = "2020 Virtual Reality",
-                    Order = 2,
-                    Description = "Explore The World",
-                }
+                     Sliders = await _context.Sliders.OrderBy(s=>s.Order).ToListAsync()
             };
+           
+         
+           await _context.SaveChangesAsync();
 
-            var model = new HomeViewModel
-            {
-                Products = products,
-                Categories = categories,
-                Sliders = sliders
-                
-            };
-            _context.SaveChanges();
-
-            return View(model);
+            return View(homeVM);
         }
     }
 }
